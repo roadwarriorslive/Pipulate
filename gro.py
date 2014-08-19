6dba820a075838d1ea17530df2cbf50278a721d7
Mike Levin <miklevin@gmail.com>
2014-08-19 17:04:38 -0600

    Identifying required parameters on functions

--- gro.py
+++ gro.py
@@ -6,6 +6,7 @@ def main():
   # https://www.youtube.com/watch?v=9jbTN9I-ce0 And made a dict of dicts
   try:
     # https://www.youtube.com/watch?v=iZOKsHzDaWg For efficient iteration
+    # https://www.youtube.com/watch?v=efL6qgVxfz0 Defined meaning of these dicts
     s['0'] = {1:'foo',2:'bar',3:'Lumberjack'}
     s['Hello'] = {'foo':'Hello','bar':'World','Lumberjack':'?'}
     s['Spam'] = {'foo':'Spam','bar':'Eggs','Lumberjack':'?'}
@@ -17,21 +18,34 @@ def main():
   for item in s['0']:
     fname = s['0'][item]
     if fname in globals():
-      print("%s is a function" % (fname))
-    else:
-      print("%s is not a function" % (fname))
+      from inspect import signature, _empty
+      # https://www.youtube.com/watch?v=WLW1-G56q1c Get the function's args
+      sig = signature(eval(fname))
+      print("%s is a function with arguments %s" % (fname, sig))
+      for param in sig.parameters.values():
+        pname = param.name
+        pdefault = param.default
+        #print(pdefault is _empty)
+        #print('%s %s' % (pname, pdefault))
+        if pdefault is _empty:
+          print('Required parameter: %s %s' % (fname, pname))
+        else:
+          print('I have default value for: %s %s %s' % (fname, pname, pdefault))
 
+  # https://www.youtube.com/watch?v=iZOKsHzDaWg Efficiently iterating
   for item in s:
     if item != '0':
       print("%s: %s" % (item, s[item]))
 
 def delrow(s, rowkey):
+  # https://www.youtube.com/watch?v=5oCRfOndrvY Deleting item from dictionary
+  # https://www.youtube.com/watch?v=fe5QFldVzh4 Breaking into its own function
   try:
     del s[rowkey]
   except:
     pass
 
-def Lumberjack():
+def Lumberjack(job, play='', status='Okay'):
   return "I'm okay"
 
 # https://www.youtube.com/watch?v=G8T1BmIw3Hs Created point-of-entry for main
