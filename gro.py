eaffdf1424983de9a79db9542e32d712fdf77b38
Mike Levin <miklevin@gmail.com>
2014-08-19 09:54:42 -0600

    Added a main function

--- gro.py
+++ gro.py
@@ -1,18 +1,22 @@
-import shelve
-# https://www.youtube.com/watch?v=RQPBXNUwsRM Make our data persistent
-s = shelve.open('drows.db')
-try:
-  # https://www.youtube.com/watch?v=q1kLEat9iFw Created first dict
-  # https://www.youtube.com/watch?v=9jbTN9I-ce0 And made a dict of dicts
-  s['drows'] = {
-    'foo':{1:'foo',2:'bar',3:'Lumberjack',4:'I work all day'},
-    'bar':{1:'foo',2:'bar',3:'Lumberjack',4:'I work all day'}
-    }
-finally:
-  s.close()
+def main():
+  import shelve
+  # https://www.youtube.com/watch?v=RQPBXNUwsRM Make our data persistent
+  s = shelve.open('drows.db')
+  try:
+    # https://www.youtube.com/watch?v=q1kLEat9iFw Created first dict
+    # https://www.youtube.com/watch?v=9jbTN9I-ce0 And made a dict of dicts
+    s['drows'] = {
+      'foo':{1:'foo',2:'bar',3:'Lumberjack',4:'I work all day'},
+      'bar':{1:'foo',2:'bar',3:'Lumberjack',4:'I work all day'}
+      }
+  finally:
+    s.close()
 
-s = shelve.open('drows.db')
-drow = s['drows']
+  s = shelve.open('drows.db')
+  drow = s['drows']
 
-for item in drow:
-  print(drow[item])
+  for item in drow:
+    print(drow[item])
+
+if __name__ == "__main__":
+  main()
