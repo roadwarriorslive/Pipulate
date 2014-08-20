d0aed5ca2abd3a5e654a8296ebb37aa2c03d7d92
Mike Levin <miklevin@gmail.com>
2014-08-20 16:25:00 -0600

    Added glob.py for a global namespace

--- gro.py
+++ gro.py
@@ -1,14 +1,38 @@
-def main():
-  import shelve
-  allrows = shelve.open('drows.db')
-  try:
-    allrows['0'] = {1:'foo',2:'bar',3:'Lumberjack',4:'Knights'}
-    allrows['Hello'] = {'foo':'Hello','bar':'World','Lumberjack':'?','Knights':'?'}
-    allrows['Spam'] = {'foo':'Spam','bar':'Eggs','Lumberjack':'?','Knights':'?'}
-  finally:
-    allrows.close()
+import glob
 
-  allrows = shelve.open('drows.db')
+def main():
+
+  allrows = ''
+  if glob.DBSOURCE == 'local':
+    import shelve
+    allrows = shelve.open('drows.db')
+    try:
+      allrows['0'] = {1:'foo',2:'bar',3:'Lumberjack',4:'Knights'}
+      allrows['Hello'] = {'foo':'Hello','bar':'World','Lumberjack':'?','Knights':'?'}
+      allrows['Spam'] = {'foo':'Spam','bar':'Eggs','Lumberjack':'?','Knights':'?'}
+    finally:
+      allrows.close()
+    allrows = shelve.open('drows.db')
+  elif glob.DBSOURCE == 'gdocs':
+    import shelve
+    allrows = shelve.open('drows.db')
+    try:
+      allrows['0'] = {1:'foo',2:'bar',3:'Lumberjack',4:'Knights'}
+      allrows['Hello'] = {'foo':'Hello2','bar':'World','Lumberjack':'?','Knights':'?'}
+      allrows['Spam'] = {'foo':'Spam2','bar':'Eggs','Lumberjack':'?','Knights':'?'}
+    finally:
+      allrows.close()
+    allrows = shelve.open('drows.db')
+  else:
+    import shelve
+    allrows = shelve.open('drows.db')
+    try:
+      allrows['0'] = {1:'foo',2:'bar',3:'Lumberjack',4:'Knights'}
+      allrows['Hello'] = {'foo':'Hello3','bar':'World','Lumberjack':'?','Knights':'?'}
+      allrows['Spam'] = {'foo':'Spam3','bar':'Eggs','Lumberjack':'?','Knights':'?'}
+    finally:
+      allrows.close()
+    allrows = shelve.open('drows.db')
 
   fargs = {}
   for item in allrows['0']:
@@ -17,7 +41,7 @@ def main():
       fargs[fname] = {}
       from inspect import signature, _empty
       sig = signature(eval(fname))
-      print("%s is a function with arguments %s" % (fname, sig))
+      # print("%s is a function with arguments %s" % (fname, sig))
       for param in sig.parameters.values():
         pname = param.name
         pdefault = param.default
@@ -25,11 +49,11 @@ def main():
         #print('%s %s' % (pname, pdefault))
         if pdefault is _empty:
           fargs[fname][pname] = None
-          print('Required parameter: %s %s' % (fname, pname))
+          # print('Required parameter: %s %s' % (fname, pname))
         else:
           fargs[fname][pname] = pdefault
-          print('I have default value for: %s %s %s' % (fname, pname, pdefault))
-  print(fargs)
+          #print('I have default value for: %s %s %s' % (fname, pname, pdefault))
+  # print(fargs)
 
   for item in allrows:
     if item != '0':
