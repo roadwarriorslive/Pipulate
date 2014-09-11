681541881e787b7cb1ad8ca3f7c7f3149447bbe2
Mike Levin <miklevin@gmail.com>
2014-09-11 08:40:06 -0600

    Sticking closer to PEP 257 Docstring Conventions

--- gro.py
+++ gro.py
@@ -1,9 +1,11 @@
 import globs
 
 def main():
-  '''This main function could have been the entire dosheet function, but this
+  """Allows processing of multiple worksheets.
+
+  This main function could have been the entire dosheet function, but this
   extra level allows for looping trough multiple worksheet sources and serves
-  as a hook for future automation like scheduled tasks.'''
+  as a hook for future automation like scheduled tasks."""
   funcs = [x for x in globals().keys() if x[:2] != '__']
   globs.funcslc = [x.lower() for x in funcs]
   globs.transfunc = dict(zip(globs.funcslc, funcs)) 
@@ -12,12 +14,14 @@ def main():
     print()
 
 def dosheet(dbsource):
-  '''The purpose of this function is to feed Python lists representing rows of
-  a spreadsheet into the processrow function, which handles question mark
+  """Steps through active worksheet feeding each row for processing.
+  
+  The purpose of this function is to feed Python lists representing rows of a
+  spreadsheet into the processrow function, which handles question mark
   replacement. This is the outer loop of that process representing the entire
   spreadsheet. In the first case, data can be loaded into a shelve object from
   csv and other sources for processing large datasets and scheduled tasks, or
-  from Google Spreadsheets for lesser data, but a more interactive approach.'''
+  from Google Spreadsheets for lesser data, but a more interactive approach."""
   allrows = ''
   if dbsource == 'local':
     import shelve, csv
@@ -48,12 +52,14 @@ def dosheet(dbsource):
   else:
     pass
 
-def dorow(rownum, arow):
-  '''Called on each row of a worksheet and either initializes functions when
+def processrow(rownum, arow):
+  """Separates row-1 handling from question mark detection on all other rows.
+  
+  Called on each row of a worksheet and either initializes functions when
   it's row 1, or steps cell by cell along each subsequent (fed-in) row and when
   encountering a question mark, it determines whether to invoke the function
   indicated by the column label, using values from the active row as parameter
-  values if available, parameter defaults if not, and None if no defaults.'''
+  values if available, parameter defaults if not, and None if no defaults."""
   if rownum == '1':
     globs.row1 = [x.lower() for x in arow]
     row1funcs(arow)
@@ -63,55 +69,13 @@ def dorow(rownum, arow):
         if acell == '?':
           evalfunc(coldex, arow)
 
-def evalfunc(coldex, arow):
-  '''Once a question mark is found in a cell belonging to a function-named
-  column, the exact invocation that's needed must be built, keeping in mind
-  default values provided by function itself, as well as parameter values
-  provided in the provided row. Because rows are being handled as lists, order
-  is important, and the column index allows function name lookup.'''
-  fname = globs.transfunc[globs.row1[coldex]]
-  fargs = globs.fargs[coldex]
-  evalme = "%s(" % fname
-  if fargs:
-    for anarg in fargs:
-      anarg = anarg.lower()
-      argval = getargval(anarg, fargs[anarg], arow)
-      evalme = "%s%s=%s, " % (evalme, anarg, argval)
-    evalme = evalme[:-2] + ')'
-  else:
-    evalme = evalme + ')'
-  print('%s: %s' % (evalme, eval(evalme)))
-
-def getargval(anarg, defargval, arow):
-  '''This function returns which value should be used as the arguments to the
-  function invocation string being built. The value found on the row always
-  beats the default provided by the function. Lacking values on the row and a
-  default, the Python value of None will be returned.'''
-  for coldex, acol in enumerate(globs.row1):
-    if acol == anarg:
-      if arow[coldex]:
-        return adq(arow[coldex])
-  if defargval:
-    return adq(defargval)
-  else:
-    return None
-  
-def adq(aval):
-  '''This handles the value quoting details when building argument part of the
-  function invocation string when replacing a question mark. For example, the
-  keyword None must not become quoted. Typically, numbers shouldn't be quoted
-  either, but we're feeding everything but None around as strings for now.'''
-  if aval == None:
-    return None
-  else:
-    return "'%s'" % (aval)
-
 def row1funcs(arow):
-  '''This is only invoked on row 1 of a spreadsheet, where the names of
-  functions and parameters are expected to be discovered. By the end, we've
-  created a dictionary of dictionaries called globs.fargs which plays an
-  important role in building the code necessary for question mark
-  replacement.'''
+  """Scans row-1 for names of global functions and builds dict of requirements.
+  
+  This is only invoked on row 1 of a spreadsheet, where the names of functions
+  and parameters are expected to be discovered. By the end, we've created a
+  dictionary of dictionaries called globs.fargs which plays an important role
+  in building the code necessary for question mark replacement."""
   fargs = {}
   for coldex, fname in enumerate(arow):
     if fname.lower() in globs.funcslc:
@@ -126,6 +90,55 @@ def row1funcs(arow):
         else:
           fargs[coldex][pname] = pdefault
   globs.fargs = fargs
+
+def evalfunc(coldex, arow):
+  """Builds string to execute to get value to replace question mark with.
+  
+  Once a question mark is found in a cell belonging to a function-named column,
+  the exact invocation that's needed must be built, keeping in mind default
+  values provided by function itself, as well as parameter values provided in
+  the provided row. Because rows are being handled as lists, order is
+  important, and the column index allows function name lookup."""
+  fname = globs.transfunc[globs.row1[coldex]]
+  fargs = globs.fargs[coldex]
+  evalme = "%s(" % fname
+  if fargs:
+    for anarg in fargs:
+      anarg = anarg.lower()
+      argval = getargval(anarg, fargs[anarg], arow)
+      evalme = "%s%s=%s, " % (evalme, anarg, argval)
+    evalme = evalme[:-2] + ')'
+  else:
+    evalme = evalme + ')'
+  print('%s: %s' % (evalme, eval(evalme)))
+
+def getargval(anarg, defargval, arow):
+  """Returns value to set argument equal-to in function invocation string.
+  
+  This function returns which value should be used as the arguments to the
+  function invocation string being built. The value found on the row always
+  beats the default provided by the function. Lacking values on the row and a
+  default, the Python value of None will be returned."""
+  for coldex, acol in enumerate(globs.row1):
+    if acol == anarg:
+      if arow[coldex]:
+        return adq(arow[coldex])
+  if defargval:
+    return adq(defargval)
+  else:
+    return None
+  
+def adq(aval):
+  """Conditionally builds quotes on arg-value for function invocation string.
+  
+  This handles the value quoting details when building argument part of the
+  function invocation string when replacing a question mark. For example, the
+  keyword None must not become quoted. Typically, numbers shouldn't be quoted
+  either, but we're feeding everything but None around as strings for now."""
+  if aval == None:
+    return None
+  else:
+    return "'%s'" % (aval)
 
 def Func1():
   return "No arguments here"
