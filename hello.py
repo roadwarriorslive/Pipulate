39e7722c10b6179c74cc5cc18d357c2c85b38931
Mike Levin <miklevin@gmail.com>
2014-09-18 10:36:56 -0600

    Added request object for URL parameter access

--- hello.py
+++ hello.py
@@ -2,10 +2,14 @@ from flask import Flask
 app = Flask(__name__)
 
 from pipulate import *
+from flask import request
 
 @app.route("/")
 def hello():
-  main() 
+  if "go" in request.args:
+    main() 
+    return "Replaced questionmarks"
+  return "Doing nothing"
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', port=8080)
