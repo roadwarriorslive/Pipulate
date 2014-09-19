b6a50fc0e06765974da91234950925385f955c81
Mike Levin <miklevin@gmail.com>
2014-09-19 08:49:09 -0600

    Got web forms displaying

--- pipulate.py
+++ pipulate.py
@@ -33,6 +33,20 @@ app = Flask(__name__)
 from flask import request
 from flask import render_template
 
+from flask_wtf import Form
+from wtforms import StringField
+from wtforms.validators import DataRequired
+
+class MyForm(Form):
+  name = StringField('name', validators=[DataRequired()])
+
+@app.route('/submit', methods=('GET', 'POST'))
+def submit():
+    form = MyForm()
+    if form.validate_on_submit():
+        return redirect('/success')
+    return render_template('submit.html', form=form)
+
 @app.route("/", methods=['GET', 'POST'])
 def hello():
   if request.method == 'POST':
@@ -45,7 +59,8 @@ def hello():
         main() 
         return "Replaced questionmarks"
     else:
-      return render_template('pipulate.html', name="Mike")
+      form = MyForm(csrf_enabled=False)
+      return render_template('pipulate.html', form=form, name="Mike")
 
 def main():
   """Allows processing of multiple worksheets.
