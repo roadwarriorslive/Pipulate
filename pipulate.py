38446cfd5d771287292b61cdaeb7dc4730941691
Mike Levin <miklevin@gmail.com>
2014-12-12 12:40:25 -0500

    Added Tweets function to get tweet count on any given url

--- pipulate.py
+++ pipulate.py
@@ -343,6 +343,13 @@ def Func1():
 def Func2(param1, param2='', status='Okay'):
   return "%s %s" % (param1, param2)
 
+def tweets(url):
+  import urllib2, json
+  api = "http://urls.api.twitter.com/1/urls/count.json?url="
+  respobj = urllib2.urlopen(api + url)
+  adict = json.load(respobj)
+  return adict["count"]
+
 #if __name__ == "__main__":
 #  main()
 
