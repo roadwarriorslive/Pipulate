1a54de24f205ea3ac5caa6c030c37b6ccf02d3b6
Mike Levin <miklevin@gmail.com>
2015-02-04 14:33:10 +0000

    Took out marketing text from main file

--- pipulate.py
+++ pipulate.py
@@ -9,29 +9,6 @@
                | |
                |_|
 
-Got a list that you need to look up something for every item - like grabbing
-title tags for a list of URLs? Well, then Pipulate is your answer! For example,
-for any given URL, you will be able to:
-
-  1. Record the number of times that URL has appeared in a Tweet
-  2. Record the number of times that URL was liked or shared in Facebook
-  3. Record the number of times that URL was +1'd in Google Plus
-  4. Figure out what keyword the page is trying to target for search traffic
-  5. See how well that URL is doing for that keyword (position tracking)
-
-And it all accumulates on a spreadsheet where you can do trending and keep
-track of progress - all for free! But Pipulate is so much more, able to do such
-lookups using any source data (addresses, keywords, etc.) against any Web or
-data API source (you have access to), allowing you to write your own Python
-functions to extend the system. And what system is that? You can learn it well,
-because almost every line of code accompanied by a YouTube video. Playlist:
-https://www.youtube.com/watch?v=SdzDaohx-GA&list=PLy-AlqZFg6G8tBTB6FFN68mryG4JlCaf-
-
-REQUIREMENTS:
-easy_install flask
-easy_install flask_wtf
-easy_install requests
-easy_install gspread
 """
 
 import globs #Create objects that don't have to be passed as arguments.
