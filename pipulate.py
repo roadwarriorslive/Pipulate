5260e31db61b00b1610e9e7005d6d5fd1efc6f7a
Mike Levin <miklevin@gmail.com>
2015-02-10 18:30:14 +0000

    more silly edits

--- pipulate.py
+++ pipulate.py
@@ -1,4 +1,4 @@
-""" Pipulate lets you collect data straight off of the Web into spreadsheets.
+""" 
           _____ _             _       _
          |  __ (_)           | |     | |
          | |__) | _ __  _   _| | __ _| |_ ___     ___ ___  _ __ ___
