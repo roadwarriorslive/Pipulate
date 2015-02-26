e5a3bee596d66442d2c0fc655a9d1b8f496a250f
Mike Levin <miklevin@gmail.com>
2015-02-26 21:46:32 +0000

    Gave unique pid's to daemons and changed mv to cp in install.sh

--- install.sh
+++ install.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 # daemons
-mv systuff/daemons/webpipulate /etc/init.d
-mv systuff/daemons/loopipulate /etc/init.d
+cp systuff/daemons/webpipulate /etc/init.d
+cp systuff/daemons/loopipulate /etc/init.d
 chmod 755 /etc/init.d/webpipulate
 chmod 755 /etc/init.d/loopipulate
 update-rc.d webpipulate enable
 update-rc.d loopipulate enable
 
 # cron jobs
-mv systuff/crons/webpipulate /etc/cron.hourly
-mv systuff/crons/loopipulate /etc/cron.hourly
+cp systuff/crons/webpipulate /etc/cron.hourly
+cp systuff/crons/loopipulate /etc/cron.hourly
 chmod 755 /etc/cron.hourly/webpipulate
 chmod 755 /etc/cron.hourly/loopipulate
 
 # easy-dev
-mv systuff/sbins/pipulte /usr/local/sbin
+cp systuff/sbins/pipulte /usr/local/sbin
 chmod 755 /usr/local/sbin
 
