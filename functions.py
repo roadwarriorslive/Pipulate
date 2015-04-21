822e70526beaff1e4f2748373ad123a4b5023ce4
Mike Levin <miklevin@gmail.com>
2015-04-21 18:54:02 +0000

    Added PageRank function April 21st 2015 about 2:54 PM

--- functions.py
+++ functions.py
@@ -29,14 +29,14 @@ def walkdict(obj, key):
         return v
   return None
 
-def mobilecanonical(mobile):
+def mcanonical(mobile):
   return mobile
 
 def mobilicious(alternate, altcanonical):
   return "%s : %s" % (alternate, altcanonical)
 
 def crawl(url):
-  fcols = ['Depth', 'Title', 'Description', 'PageRank', 'Mobile', 'MobileCanonical', 'Mobilicious']
+  fcols = ['Depth', 'Title', 'Description', 'PageRank', 'Mobile', 'Mcanonical', 'Mobilicious']
   therange = 'B1:%s2' % globs.letter[len(fcols)+1]
   CellList = globs.sheet.range(therange)
   vals = fcols + ['0'] + ['?']*(len(fcols)-1)
@@ -158,6 +158,61 @@ def likes(url):
   respobj = requests.get('https://graph.facebook.com/' + url) 
   adict = respobj.json()
   return walkdict(adict, 'likes')
+
+def pagerank(url):
+  import urllib
+  def CheckHash(HashInt):
+    HashStr = "%u" % (HashInt)
+    Flag = 0
+    CheckByte = 0
+    i = len(HashStr) - 1
+    while i >= 0:
+      Byte = int(HashStr[i])
+      if 1 == (Flag % 2):
+        Byte *= 2;
+        Byte = Byte / 10 + Byte % 10
+      CheckByte += Byte
+      Flag += 1
+      i -= 1
+    CheckByte %= 10
+    if 0 != CheckByte:
+      CheckByte = 10 - CheckByte
+      if 1 == Flag % 2:
+        if 1 == CheckByte % 2:
+          CheckByte += 9
+        CheckByte >>= 1
+    return '7' + str(CheckByte) + HashStr
+  def HashURL(Str):
+    def  IntStr(String, Integer, Factor):
+      for i in range(len(String)) :
+        Integer *= Factor
+        Integer &= 0xFFFFFFFF
+        Integer += ord(String[i])
+      return Integer
+    C1 = IntStr(Str, 0x1505, 0x21)
+    C2 = IntStr(Str, 0, 0x1003F)
+    C1 >>= 2
+    C1 = ((C1 >> 4) & 0x3FFFFC0) | (C1 & 0x3F)
+    C1 = ((C1 >> 4) & 0x3FFC00) | (C1 & 0x3FF)
+    C1 = ((C1 >> 4) & 0x3C000) | (C1 & 0x3FFF)
+    T1 = (C1 & 0x3C0) << 4
+    T1 |= C1 & 0x3C
+    T1 = (T1 << 2) | (C2 & 0xF0F)
+    T2 = (C1 & 0xFFFFC000) << 4
+    T2 |= C1 & 0x3C00
+    T2 = (T2 << 0xA) | (C2 & 0xF0F0000)
+    return (T1 | T2)
+  hsh = CheckHash(HashURL(url))
+  gurl = 'http://toolbarqueries.google.com/tbr?client=navclient-auto&features=Rank:&q=info:%s&ch=%s' % (urllib.quote(url), hsh)
+  f = urllib.urlopen(gurl)
+  st = f.read()
+  st = st.lstrip().rstrip()
+  st = st[9:]
+  try:
+    st = int(st)
+  except:
+    st = 0
+  return st
 
 behaviors = {
   'Get Links' : crawlinit
