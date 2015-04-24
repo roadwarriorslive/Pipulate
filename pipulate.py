21e046c14bede73dd5019ed190bfb8e0f97025d8
Mike Levin <miklevin@gmail.com>
2015-04-24 22:29:44 +0000

    Media query for larger desktops

--- pipulate.py
+++ pipulate.py
@@ -141,11 +141,20 @@ def main():                                               # visiting app's homep
         form.pipurl.data = session['u']
   out("Selecting template method.")
   if STREAMIT:
+    #Handle streaming user interface updates resulting from a POST method call.
     return Response(stream_template('pipulate.html', form=form, data=STREAMIT, invokemode="Pipulate"))
   else:
+    #Handle non-streaming user interface build resulting from a GET method call.
     out("EXITING MAIN FUNCTION RENDER", "0", '-')
     showbutton = globs.modes.get(INVOKEMODE,'Get Links')
-    return render_template('pipulate.html', form=form, invokemode=showbutton)
+
+    menu = """        <select name="options">
+              <option>foo</option>
+              <option>bar</option>
+              <option>spam</option>
+              <option>eggs</option>
+            </select>"""
+    return render_template('pipulate.html', form=form, invokemode=showbutton, widget=menu)
   out("EXITING MAIN", "0", '-')
 
 def LogUser(authkey):
