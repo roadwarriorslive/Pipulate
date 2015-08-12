e5658730f17629e00da1a25767b395df5c01055d
Mike Levin <miklevin@gmail.com>
2015-08-12 16:48:45 +0000

    Swapped out poem in Webpipulate with one of my own

--- webpipulate.py
+++ webpipulate.py
@@ -4,11 +4,27 @@
 #     \ V  V /  __/ |_) |  ___) | | | | | | | | | |
 #      \_/\_/ \___|_.__/  |____/|_| |_|_|_| |_| |_|
 #
-#  What brought the kindred spider to that height,
-# Then steered the white moth thither in the night?
-#      What but design of darkness to appall?--
-#       If design govern in a thing so small.
-#              -- Robert Frost, Design
+#    The 80/20 friggn’ rule —
+#    Gotta’ use it. It’s a tool
+#    Reducing clutter that we pack
+#    Into our lives to hold us back.
+#    
+#    When goals elude and time flies past
+#    And with each step, you’re still in last
+#    Consider how you lost your aim
+#    You’ll find recursive tasks to blame.
+#    
+#    With each thing you set to do,
+#    A hundred more come fly at you.
+#    Until at last, you quite forget
+#    The goal for which you had first set.
+#    
+#    The 80/20 Rule just asks
+#    That you should start to plan your tasks
+#    So when you’re only one-fifth done
+#    You could have stopped and still have won.
+#
+#    --December 7, 2002, Mike Levin
 
 from pipulate import *
 import os
