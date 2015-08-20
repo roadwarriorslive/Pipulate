026592a994aa239acfa4a1ecd335f4177193e274
Mike Levin <miklevin@gmail.com>
2015-08-20 21:21:50 +0000

    The Form Switcher is taking shape

--- pipulate.py
+++ pipulate.py
@@ -78,15 +78,18 @@ class PipForm(Form):
   magicbox = TextAreaField("magicbox")                              # This box shows the JSON data being worked upon.
   options = SelectField("options")                                  # What you're asking Pipulate to do.
 
-class SecondaryMenu(PipForm):
+class PipForm2(PipForm):
+  """Define form for interstitial options"""
+  secondary = HiddenField()
+
+class ClearSheet1(PipForm2):
+  radios = RadioField(choices=crawlchoices())
+
+class AnotherMenu(PipForm2):
   radios = RadioField(choices=crawlchoices())
   checks = SelectMultipleField(choices=crawlchoices(), 
     option_widget=widgets.CheckboxInput(), 
     widget=widgets.ListWidget(prefix_label=False))
-
-class PipForm2(SecondaryMenu):
-  """Define form for interstitial options"""
-  secondary = HiddenField()
 
 #  _____ _           _                      _
 # |  ___| | __ _ ___| | __  _ __ ___   __ _(_)_ __
@@ -109,7 +112,8 @@ def main():                                                         # of entry "
   stop = False                                                      # Pipulate "stops" unless permitted to continue.
   streamit = False                                                  # Controls Flask's stream versus render template.
   form = PipForm(csrf_enabled=False)                                # All WTForms are instances of classes. Main form.
-  form2 = PipForm2(csrf_enabled=False)
+  formSwitch = {'clear': ClearSheet1(csrf_enabled=False)}
+  form2 = formSwitch['clear']
   pipstate = None
   menudefault = None
   configform = ConfigForm(csrf_enabled=False)                       # The form to let you 1st time configure server.
