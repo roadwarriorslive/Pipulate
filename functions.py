c377411cdc5071b56381ccc427b959148f692e66
Mike Levin <miklevin@gmail.com>
2015-08-31 05:51:48 +0000

    Okay, we're getting somewhere

--- functions.py
+++ functions.py
@@ -683,10 +683,93 @@ def makeview(viewname):
     'sharelink': 'http://%s/v?k=%s&v=%s' % (globs.HOST, globs.DOCID, mygid),
     'datestamp': datestamp(), 
     'guid': mygid, 
-    'includecode': '<link rel="1"/>\n<link rel="2"/>', 
-    'compresseddata': 'spamandeggs'
+    'includecode': '<script src="http://js.cytoscape.org/js/cytoscape.min.js"></script>',
+    'compresseddata': sampleData()
     }
   return "%s done" % viewname, rowdict
 
 def foo():
   return 'bar', {'first':'one', 'second':'two', 'third': 'three'}
+
+def sampleData():
+  return '''<script>
+$(function(){ // on dom ready
+
+$('#cy').cytoscape({
+  style: cytoscape.stylesheet()
+    .selector('node')
+      .css({
+        'content': 'data(name)',
+        'text-valign': 'center',
+        'color': 'white',
+        'text-outline-width': 2,
+        'text-outline-color': '#888'
+      })
+    .selector('edge')
+      .css({
+        'target-arrow-shape': 'triangle'
+      })
+    .selector(':selected')
+      .css({
+        'background-color': 'black',
+        'line-color': 'black',
+        'target-arrow-color': 'black',
+        'source-arrow-color': 'black'
+      })
+    .selector('.faded')
+      .css({
+        'opacity': 0.25,
+        'text-opacity': 0
+      }),
+  
+  elements: {
+    nodes: [
+      { data: { id: 'j', name: 'Jerry' } },
+      { data: { id: 'e', name: 'Elaine' } },
+      { data: { id: 'k', name: 'Kramer' } },
+      { data: { id: 'g', name: 'George' } }
+    ],
+    edges: [
+      { data: { source: 'j', target: 'e' } },
+      { data: { source: 'j', target: 'k' } },
+      { data: { source: 'j', target: 'g' } },
+      { data: { source: 'e', target: 'j' } },
+      { data: { source: 'e', target: 'k' } },
+      { data: { source: 'k', target: 'j' } },
+      { data: { source: 'k', target: 'e' } },
+      { data: { source: 'k', target: 'g' } },
+      { data: { source: 'g', target: 'j' } }
+    ]
+  },
+  
+  layout: {
+    name: 'grid',
+    padding: 10
+  },
+  
+  // on graph initial layout done (could be async depending on layout...)
+  ready: function(){
+    window.cy = this;
+    
+    // giddy up...
+    
+    cy.elements().unselectify();
+    
+    cy.on('tap', 'node', function(e){
+      var node = e.cyTarget; 
+      var neighborhood = node.neighborhood().add(node);
+      
+      cy.elements().addClass('faded');
+      neighborhood.removeClass('faded');
+    });
+    
+    cy.on('tap', function(e){
+      if( e.cyTarget === cy ){
+        cy.elements().removeClass('faded');
+      }
+    });
+  }
+});
+
+}); // on dom ready
+</script>'''
