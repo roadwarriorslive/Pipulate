41831638d3516ec4e4418ed2f03e5132f3fb9748
Mike Levin <miklevin@gmail.com>
2018-01-17 17:04:53 -0500

    Including Google Client ID & Secret for ease of use purposes

---
+++ pipulate/apis.py
@@ -0,0 +1,7 @@
+# I've thrown this client_id and secret to the cause so you can get Pipulate
+# working fast and easy, but you can separate your instance by creating your own
+# OAuth Client ID login (asking user for permission via browser pop-up) at
+# https://console.developers.google.com
+
+client_id =     "769904540573-knscs3mhvd56odnf7i8h3al13kiqulft.apps.googleusercontent.com"
+client_secret = "D2F1D--b_yKNLrJSPmrn2jik"
--- setup.py
+++ setup.py
@@ -4,9 +4,24 @@ setup(
   packages = ['pipulate'],
   version = '0.1',
   description = 'Manipulate data in Google Sheets',
+  long_description='Integrate Google Sheets with your Python Pandas data work.',
   author = 'Mike Levin, SEO',
-  author_email = 'pipulate@gmail.com',
+  license='MIT',
+  author_email = 'miklevin@gmail.com',
   url = 'https://github.com/miklevin/pipulate',
   download_url = 'https://github.com/miklevin/pipulate/archive/0.1.tar.gz',
   keywords = ['data', 'datascience', 'spreadsheet', 'pandas', 'gspread', 'gdata', 'seo']
 )
+
+classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Science/Research',
+	'Topic :: Databasea',
+     'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.2',
+    'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.7'
+]
