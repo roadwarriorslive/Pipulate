8618c3d82d4797bf74cd1e74b02f8691a03d667c
Mike Levin <miklevin@gmail.com>
2018-07-17 10:16:30 -0400

    journal & readme commit

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'pipulate',
   packages = ['pipulate'],
-  version = '0.1.9',
+  version = '0.1.91',
   download_url = 'https://github.com/miklevin/pipulate/archive/0.1.9.tar.gz',
   description = 'Use Google Sheets for Marketing Dashboards Free',
   long_description='Light framework to help do manipulate Google Sheets as Pandas Dataframes. Useful for SEO and Social Media Dashboards.',
