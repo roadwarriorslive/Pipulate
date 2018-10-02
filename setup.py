e40a06864e7b84e7db1a80f544613236a0faf9ab
Mike Levin <miklevin@gmail.com>
2018-10-02 16:02:05 -0400

    Revised readme for PyPI

--- setup.py
+++ setup.py
@@ -1,17 +1,18 @@
 from distutils.core import setup
 setup(
-  name = 'pipulate',
-  packages = ['pipulate'],
-  version = '0.2.2',
-  download_url = 'https://github.com/miklevin/pipulate/archive/0.2.2.tar.gz',
-  description = 'Update Google Sheets for Data Dashboards (alt to Tableau & Data Studio).',
-  author = 'Mike Levin SEO, 360i & Commodore alum, Creator of HitTail now working in NYC for J2 Ziff-Davis Mashable IGN.',
-  license='MIT',
-  author_email = 'miklevin@gmail.com',
-  url = 'https://github.com/miklevin/pipulate',
-  python_requires='>=3.6',
-  install_requires=['gspread', 'httplib2', 'google-api-python-client'],
-  keywords = ['seo', 'google sheets', 'data', 'datamaster', 'data master', 'google', 'database', 'sheets', 'pandas', 'gspread', 'google data', 'google spreadsheets', 'google sheet', 'google spreadsheet', 'spreadsheets', 'spreadsheet', 'databases', 'datascience', 'data science']
+    name = 'pipulate',
+    packages = ['pipulate'],
+    version = '0.2.22',
+    download_url = 'https://github.com/miklevin/pipulate/archive/0.2.22.tar.gz',
+    description = 'Update Google Sheets for Data Dashboards (alt to Tableau & Data Studio).',
+    long_description = open('README.rst', 'r').read(),
+    author = 'Mike Levin SEO, 360i & Commodore alum, Creator of HitTail now working in NYC for J2 Ziff-Davis Mashable IGN.',
+    license='MIT',
+    author_email = 'miklevin@gmail.com',
+    url = 'https://github.com/miklevin/pipulate',
+    python_requires='>=3.6',
+    install_requires=['gspread', 'httplib2', 'google-api-python-client'],
+    keywords = ['seo', 'google sheets', 'data', 'datamaster', 'data master', 'google', 'database', 'sheets', 'pandas', 'gspread', 'google data', 'google spreadsheets', 'google sheet', 'google spreadsheet', 'spreadsheets', 'spreadsheet', 'databases', 'datascience', 'data science']
 )
 
 classifiers=[
