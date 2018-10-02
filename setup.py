ff8869638f5340d0bab2e44663d660654daa962f
Mike Levin <miklevin@gmail.com>
2018-10-02 14:19:18 -0400

    Preparing for PyPI

--- setup.py
+++ setup.py
@@ -2,10 +2,9 @@ from distutils.core import setup
 setup(
   name = 'pipulate',
   packages = ['pipulate'],
-  version = '0.2',
-  download_url = 'https://github.com/miklevin/pipulate/archive/0.1.2.tar.gz',
+  version = '0.2.2',
+  download_url = 'https://github.com/miklevin/pipulate/archive/0.2.2.tar.gz',
   description = 'Update Google Sheets for Data Dashboards (alt to Tableau & Data Studio).',
-  long_description = open('README-PyPI.rst').read(),
   author = 'Mike Levin SEO, 360i & Commodore alum, Creator of HitTail now working in NYC for J2 Ziff-Davis Mashable IGN.',
   license='MIT',
   author_email = 'miklevin@gmail.com',
