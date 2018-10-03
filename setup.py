e0ec4a3aa5ff77afa0d78569b10783c51fc95345
Mike Levin <miklevin@gmail.com>
2018-10-03 18:35:35 -0400

    Still trying to get pip install to work from Jupyter Notebook

--- setup.py
+++ setup.py
@@ -1,18 +1,22 @@
 from distutils.core import setup
+
+version = '0.2.29'
+download_url = 'https://github.com/miklevin/pipulate/archive/%s.tar.gz' % version
+
 setup(
     name = 'pipulate',
     packages = ['pipulate'],
-    version = '0.2.27',
-    download_url = 'https://github.com/miklevin/pipulate/archive/0.2.25.tar.gz',
-    description = 'Free and Open Source SEO Software (update Google Sheets for Data Dashboards).',
-    long_description = open('./README.rst', 'r').read(),
-    author = 'Mike Levin SEO in NYC, alum of Commodore & 360i, Creator of HitTail, currently with J2/Ziff-Davis/IGN/Mashable.',
+    version = version,
+    download_url = download_url,
+    description = 'Free and Open Source SEO Software (Auto-Update Google Sheets).',
+    long_description = open('README.rst', 'r').read(),
+    author = 'Mike Levin SEO in NYC, alum of Commodore & 360i, Creator of HitTail.com, currently with J2/Ziff-Davis/IGN/Mashable.',
     license='MIT',
     author_email = 'miklevin@gmail.com',
     url = 'https://github.com/miklevin/pipulate',
     python_requires='>=3.6',
     install_requires=['gspread', 'httplib2', 'google-api-python-client'],
-    keywords = ['seo', 'google sheets', 'data', 'datamaster', 'data master', 'google', 'database', 'sheets', 'pandas', 'gspread', 'google data', 'google spreadsheets', 'google sheet', 'google spreadsheet', 'spreadsheets', 'spreadsheet', 'databases', 'datascience', 'data science']
+    keywords = ['open source', 'seo software', 'google sheets', 'linux scheduling', 'datamaster', 'datamasterng', 'pandas', 'gspread']
 )
 
 classifiers=[
