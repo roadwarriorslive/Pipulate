23f17c3d0bf581b16fd39669d9fa1635f03d6e76
Mike Levin <miklevin@gmail.com>
2019-08-29 16:13:28 -0400

    Got rid of misleading object names

--- pipulate/__init__.py
+++ pipulate/__init__.py
@@ -46,9 +46,9 @@ def pipulate(tab, rows, cols, columns=None):
         columns = tab.range(row1, col1, row1, col2)
         columns = [a1(x.col) for x in columns]
     df = pd.DataFrame(list_of_lists, columns=columns)
-    print('cl, df = gs.pipulate(%s, rows=%s, cols=%s) was successful.' % (tab.title, rows, cols))
+    print('cl, df = gs.pipulate(tab, rows=%s, cols=%s) was successful.' % (rows, cols))
     print("You may now manipulate the DataFrame but maintain its (%s x %s) shape." % df.shape)
-    print('To update the GSheet with changes, gs.populate("%s", cl, df)' % tab.title)
+    print('To update the GSheet with changes, gs.populate(tab, cl, df)'
     return cl, df
 
 
@@ -64,7 +64,7 @@ def populate(tab, cl, df):
         tab.update_cells(cl)
     else:
         raise SystemExit()
-    print('gs.populate(%s, cl, df) <<< GSHEET UPDATED! >>>' % tab.title)
+    print('gs.populate(tab, cl, df) <<< GSHEET UPDATED! >>>'
     print()
 
 
