a225544d9eba0d2eff1c539d8cd881a7e7226872
Mike Levin <miklevin@gmail.com>
2019-10-24 16:51:50 -0400

    Getting ready for new PyPI release

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 from distutils.core import setup
 
 # https://packaging.python.org/tutorials/packaging-projects/
+# python setup.py sdist
+# twine upload dist/*   
 
 with open("README.rst", "r") as fh:
     long_description = fh.read()
@@ -8,7 +10,7 @@ with open("README.rst", "r") as fh:
 setup(
     name = 'pipulate',
     packages = ['pipulate'],
-    version = '0.2.47',
+    version = '0.2.5',
     description = 'Free and Open Source SEO Software (Auto-Update Google Sheets).',
     long_description = long_description,
     author = 'Mike Levin SEO in NYC, alum of Commodore & 360i, Creator of HitTail.com, currently with J2/Ziff-Davis/IGN/Mashable.',
@@ -16,7 +18,7 @@ setup(
     author_email = 'miklevin@gmail.com',
     url = 'https://github.com/miklevin/pipulate',
     python_requires='>=3.6',
-    install_requires=['pandas', 'gspread', 'httplib2', 'google-api-python-client', 'oauth2client'],
+    install_requires=['pandas', 'gspread', 'google-api-python-client', 'google-auth-oauthlib'],
     keywords = ['open source', 'seo software', 'google sheets', 'linux scheduling', 'datamaster', 'datamasterng', 'pandas', 'gspread']
 )
 
