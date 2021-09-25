a7cb3386fb496d8c33daf60ac5d857916faa93dc
Mike Levin <miklevin@hotmail.com>
2021-09-25 01:00:56 -0400

    Rolling back to pre-ohawf

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.rst", "r") as fh:
 setup(
     name = 'pipulate',
     packages = ['pipulate'],
-    version = '0.3.59',
+    version = '0.4.00',
     description = 'Free and Open Source SEO Software (Auto-Update Google Sheets).',
     long_description = long_description,
     author = 'Mike Levin SEO in NYC, alum of Commodore & 360i, Creator of HitTail.com, currently with J2/Ziff-Davis/IGN/Mashable.',
@@ -18,7 +18,7 @@ setup(
     author_email = 'miklevin@gmail.com',
     url = 'https://github.com/miklevin/pipulate',
     python_requires='>=3.6',
-    install_requires=['pandas', 'gspread', 'pygsheets', 'google-api-python-client', 'google-auth-oauthlib', 'httplib2'],
+    install_requires=['ohawf', 'pandas', 'httpx', 'gspread', 'google-api-python-client', 'google-auth-oauthlib', 'httplib2'],
     keywords = ['seo software', 'open source seo', 'python seo', 'open source seo', 'seo software', 'open source seo software', 'automate google sheets', 'automate gsheets' 'pandas', 'gspread', 'pygsheets', 'oauth', 'google analytics', 'youtube analytics', 'google search console', 'ga', 'gsc', 'yt', 'api']
 )
 
